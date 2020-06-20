a6f42edc098bcefff200a7694cd91fd457333d25
KhoaPhamOffline <kaiba1459@gmail.com>
2020-06-20 18:31:24 +0700

    lam xong chuc nang test git

--- src/com/PhoneStoreManager/FrontEnd/FormThemSua/BanHangForm.java
+++ src/com/PhoneStoreManager/FrontEnd/FormThemSua/BanHangForm.java
@@ -20,6 +20,8 @@ public class BanHangForm extends JPanel {
 
         ChonSanPhamPanel cspbh = new ChonSanPhamPanel(0, 0, width - 555 , height);
         this.add(cspbh);
+        
+//        Test Comments t ogit
 
         HoaDonBanHang hdbh = new HoaDonBanHang(width - 550, 0, 550, height );
         this.add(hdbh);
